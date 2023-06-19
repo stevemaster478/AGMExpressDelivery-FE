c2341e6b39117d7c6f1f70e7f4cba9a21acfde65
niuf <niuffars@pm.me>
2023-06-19 10:51:31 +0200

    Login corretto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
+import { AuthModule, HttpInterceptorConfig } from '@auth0/auth0-angular';
 import { NgModule } from '@angular/core';
-import { InputTextModule } from 'primeng/inputtext';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,7 +12,6 @@ import { StatoConsegnaComponent } from './components/statoconsegna/statoconsegna
 import { RuoloComponent } from './components/ruolo/ruolo.component';
 import { IndirizzoComponent } from './components/indirizzo/indirizzo.component';
 import { PaccoComponent } from './components/pacco/pacco.component';
-import { AuthModule } from '@auth0/auth0-angular';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -34,17 +32,17 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     AuthModule.forRoot({
       domain: 'agmexpress.eu.auth0.com',
-      clientId: 'dKUca8YM6aoH5Eg2Zdl2DOW5tsZJgT'
+      clientId: 'dKUca8YM6aoH5Eg2Zdl2DOW5tsZJgT4T',
+      authorizationParams: {
+      redirect_uri: 'http://localhost:4200/dashboard'
+    },
     }),
-
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     CommonModule,
     FormsModule,
-    InputTextModule,
     ReactiveFormsModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/callback/callback.component.ts
+++ src/app/components/callback/callback.component.ts
@@ -1,43 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
+import { RuoloService } from 'src/app/services/ruolo.service';
+import { Ruolo } from 'src/app/models/ruolo.model';
 
 @Component({
   selector: 'app-callback',
   template: ` <div>Caricamento...</div> `,
 })
 export class CallbackComponent implements OnInit {
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private ruoloService: RuoloService
+  ) {}
 
   ngOnInit(): void {
-    this.auth.handleRedirectCallback().subscribe(
-      () => {
-        // Ottenere il ruolo dell'utente dai claims del token
-        this.auth.idTokenClaims$.subscribe((claims) => {
-          const userRole = claims && claims['role'];
-
-          // Reindirizzare l'utente alla dashboard corretta in base al ruolo
-          switch (userRole) {
-            case 'admin':
-              this.router.navigate(['/admin-dashboard']);
-              break;
-            case 'user':
-              this.router.navigate(['/user-dashboard']);
-              break;
-            case 'delivery-person':
-              this.router.navigate(['/delivery-dashboard']);
-              break;
-            default:
-              // Reindirizzare l'utente alla pagina di accesso se il ruolo non è valido o non è presente
-              this.router.navigate(['/login']);
-              break;
-          }
-        });
-      },
-      (error) => {
-        // Gestire l'errore durante l'autenticazione
-        console.error("Errore durante l'autenticazione: ", error);
-      }
-    );
+    this.auth.handleRedirectCallback().subscribe(() => {
+      const ruolo: Ruolo = {
+        id: 1, // ID del ruolo utente
+        nome: 'utente', // Nome del ruolo utente
+      };
+      this.ruoloService.createRuolo(ruolo).subscribe(() => {
+        this.router.navigate(['/dashboard']);
+      });
+    });
   }
 }
